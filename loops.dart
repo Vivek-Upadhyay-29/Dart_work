00e8c2f57fd432802cd95c6097c16f5b59eb47d4
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-01-13 10:43:13 +0530

    loops

---
+++ loops.dart
@@ -0,0 +1,11 @@
+void main() {
+  String value = 'Vivek';
+  for (int i = 0; i < value.length; i++) {
+    print(value[i]);
+  }
+  int i = 0;
+  while (i < value.length) {
+    print(value[i]);
+    i = i + 1;
+  }
+}
